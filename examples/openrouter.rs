2405c6326cea01a53db6d49aa89ae3d3a1a64789
Quan Hua <quanhua92@gmail.com>
2025-01-19 16:03:52 +0700

    example: open router assistant prefill

--- examples/openrouter.rs
+++ examples/openrouter.rs
@@ -3,10 +3,7 @@ use async_llm::{init_tracing, ChatMessage, ChatRequest, Error};
 async fn example_basic() -> Result<(), Error> {
     let request = ChatRequest::new(
         "meta-llama/llama-3.2-3b-instruct:free",
-        vec![
-            ChatMessage::system("You are a helpful assistant"),
-            ChatMessage::user("Who are you?"),
-        ],
+        vec![ChatMessage::system("You are a helpful assistant")],
     )
     .user("1 + 1 =");
     tracing::info!("request: \n{}", request.to_string_pretty()?);
@@ -17,10 +14,37 @@ async fn example_basic() -> Result<(), Error> {
     Ok(())
 }
 
+async fn example_assistant_prefill() -> Result<(), Error> {
+    let request = ChatRequest::new(
+        "mistralai/mistral-7b-instruct:free",
+        vec![
+            ChatMessage::system("You are a helpful assistant"),
+            ChatMessage::user("Who are you?"),
+            ChatMessage::assistant("I'm not sure, but my best guess is"),
+        ],
+    );
+    tracing::info!("request: \n{}", request.to_string_pretty()?);
+
+    let response = request.send().await?;
+    tracing::info!("response: \n{}", response.to_string_pretty()?);
+
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     dotenvy::dotenv().ok();
     init_tracing();
 
-    example_basic().await.unwrap();
+    // https://openrouter.ai/docs/requests
+    // example_basic().await?;
+    example_assistant_prefill().await?;
+    // TODO: Images & Multimodel: image_url
+    // TODO: Images & Multimodel: base64 image
+    // TODO: Tool Calls
+    // TODO: Structured outputs
+    // TODO: Prompt Caching with `cache_control` for Anthropic
+    // TODO: Transforms: https://openrouter.ai/docs/transforms
+
+    Ok(())
 }

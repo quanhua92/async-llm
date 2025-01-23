39f72ca53de2ce5d3d412a4811a5d86c2824187d
Quan Hua <quanhua92@gmail.com>
2025-01-24 01:36:53 +0700

    test: test offline files

--- tests/test_openai.rs
+++
@@ -1,8 +0,0 @@
-mod test_utils;
-use test_utils::common_tests;
-
-#[tokio::test]
-pub async fn test_chat_completions_simple() -> Result<(), String> {
-    let _ = common_tests::common_test_chat_completions_simple().await;
-    Ok(())
-}
